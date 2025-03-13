0cbd1cfa23ad3a24f6d84f1615b9b2e6b7b93aae
AmerigoBovo97 <amerigobovo@gamil.com>
2025-03-13 21:36:35 +0100

    feat: created User entity

---
+++ src/main/java/org/milestone4/ticket/platform/ticket_platform/model/User.java
@@ -0,0 +1,24 @@
+package org.milestone4.ticket.platform.ticket_platform.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tickets")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Email can not be blank")
+    private String email;
+
+    @NotBlank(message = "Password can not be blank")
+    private String password;
+}
