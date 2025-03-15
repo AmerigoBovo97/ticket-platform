02b8e604387b2c62f7b621a4e418d4c3c3fb8542
AmerigoBovo97 <amerigobovo@gamil.com>
2025-03-15 19:38:49 +0100

    feat: add TickeCategory entity

---
+++ src/main/java/org/milestone4/ticket/platform/ticket_platform/model/TicketCategory.java
@@ -0,0 +1,49 @@
+package org.milestone4.ticket.platform.ticket_platform.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+public class TicketCategory {
+    
+    @Entity
+    @Table(name = "tickets")
+    public class TicketState {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Email can not be blank")
+    private String name;
+
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String email) {
+        this.name = email;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", email='" + getName() + "'" +
+            "}";
+    }
+}
+
+}
