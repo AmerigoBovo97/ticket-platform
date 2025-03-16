f78e6c943563d01790909b61380ea961148857d1
AmerigoBovo97 <amerigobovo@gamil.com>
2025-03-16 19:21:41 +0100

    feat: add TicketController

---
+++ src/main/java/org/milestone4/ticket/platform/ticket_platform/controller/TicketController.java
@@ -0,0 +1,10 @@
+package org.milestone4.ticket.platform.ticket_platform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ticket")
+public class TicketController {
+    
+}
