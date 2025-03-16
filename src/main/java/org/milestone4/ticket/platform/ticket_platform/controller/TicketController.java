21028d8ebfe7b7c1fee4d5bdbb1d93f8e72455cf
AmerigoBovo97 <amerigobovo@gamil.com>
2025-03-16 22:58:01 +0100

    feat: add new GetMapping to TicketController

--- src/main/java/org/milestone4/ticket/platform/ticket_platform/controller/TicketController.java
+++ src/main/java/org/milestone4/ticket/platform/ticket_platform/controller/TicketController.java
@@ -1,10 +1,31 @@
 package org.milestone4.ticket.platform.ticket_platform.controller;
 
+import java.util.List;
+
+import org.milestone4.ticket.platform.ticket_platform.model.Ticket;
+import org.milestone4.ticket.platform.ticket_platform.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 @RequestMapping("/ticket")
 public class TicketController {
+    
+    @Autowired
+    private TicketService ticketService;
+
+    @GetMapping
+    public String tickets(Model model) {
+        List<Ticket> tickets = ticketService.findAll();
+        System.out.println(tickets);
+        model.addAttribute("tickets", ticketService.findAll());
+        return "tickets/index";
+    }
     
 }
