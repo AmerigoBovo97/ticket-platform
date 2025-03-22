ff8033aee098e4c71426b36c5682200b7eec3a9c
AmerigoBovo97 <amerigobovo@gamil.com>
2025-03-22 16:41:23 +0100

    hot fix: modifie import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
 INSERT INTO roles (name) VALUES ("ADMIN");
-INSERT INTO roles (name) VALUES ("USER");
+INSERT INTO roles (name) VALUES ("OPERATOR");
 
-INSERT INTO users (username, password) VALUES ('amerigo', 123), ('riccardo', 123), ('davide', 123), ('lucrezia', 123), ('paolo', 123), ('danilo', 123);
+INSERT INTO users (username, password) VALUES ('amerigo', '{noop}123'), ('riccardo', '{noop}123'), ('davide', '{noop}123'), ('lucrezia', '{noop}123'), ('paolo', '{noop}123'), ('danilo', '{noop}123');
 
-INSERT INTO role_user (role_id, user_id) VALUES (1, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6);
+INSERT INTO role_user (role_id, user_id) VALUES (1, 1), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6);
 
 INSERT INTO sates (name) VALUES ('to do'), ('in progress'), ('completed');
 
